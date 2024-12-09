096a7c96e60e2fdaabf110b57121ac64782fb36e
Aleksi Jere Matias Kosamo <s3koal03@students.osao.fi>
2024-12-09 13:35:10 +0200

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,52 @@ import * as React from "react";
 
 const IndexPage = () => {
   return (
-    <main className="w-screen h-screen text-white bg-black">
-      <div className="w-screen px-8 text-white bg-white h-14">
-        <div className="flex items-center justify-between float-right h-full text-black w-72">
-          <p className="font-semibold select-none hover:text-gray-600">Home</p>
-          <p className="font-semibold select-none hover:text-gray-600">
-            Contact
-          </p>
-          <p className="font-semibold select-none hover:text-gray-600">
-            Projects
-          </p>
-          <p className="ml-16 font-semibold select-none hover:text-gray-500">
+    <main className="w-full min-h-screen font-sans text-white bg-gray-950">
+      <nav className="fixed top-0 left-0 z-10 w-full px-8 py-4 bg-black">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-8">
+            <a href="#Home">
+              <p className="text-lg font-semibold select-none hover:text-gray-400">
+                Home
+              </p>
+            </a>
+
+            <a href="#Projects">
+              <p className="text-lg font-semibold select-none hover:text-gray-400">
+                Projects
+              </p>
+            </a>
+
+            <a href="#Contact">
+              <p className="text-lg font-semibold select-none hover:text-gray-400">
+                Contact
+              </p>
+            </a>
+          </div>
+          <p className="ml-16 text-lg font-semibold select-none hover:text-gray-400">
             EN
           </p>
         </div>
-      </div>
+      </nav>
+
+      <section className="flex flex-col items-center justify-center h-screen px-8 text-center">
+        <h1 className="mb-4 text-4xl font-bold text-white sm:text-6xl">
+          Welcome to My Portfolio
+        </h1>
+        <p className="max-w-xl mb-8 text-lg text-gray-300 sm:text-xl">
+          My name is Aleksi, an inspiring game development student.
+        </p>
+        <a
+          className="px-8 py-3 font-semibold text-white rounded-lg bg-gradient-to-t from-blue-900 to-blue-500 "
+          href="#Projects"
+        >
+          Explore My Work
+        </a>
+      </section>
     </main>
   );
 };
 
 export default IndexPage;
 
-export const Head = () => <title>Home Page</title>;
+export const Head = () => <title>Portfolio</title>;
